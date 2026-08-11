827a7ad3796043b6365e0360b7f407b0a5ff3589
naasalwaa99-nsr <naasalwaa99.com>
2026-08-11 09:20:49 +0700

    update SELASA PAGI

--- app/components/layout/FooterSection.tsx
+++ app/components/layout/FooterSection.tsx
@@ -51,8 +51,8 @@ const locale = useLocale();
 
 const companyProfile =
   locale === "en"
-    ? "/Compro Permana 2026 EN.pdf"
-    : "/Compro Permana 2026 ID.pdf";
+  ? "/Permana_Company_Profile_2026_English.pdf"
+      : "/Permana_Company_Profile_2026_Indonesia.pdf";
 
   const [formData, setFormData] = useState<FormData>({
     fullName: "",
