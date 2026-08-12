d420a7374e2fad3f36f63f805b6bc7f99392b214
bimasyudhistira-lang <bimasyudhistira@gmail.com>
2026-08-12 16:40:02 +0700

    Replace AboutContent with BackgroundCanvas in AboutPage

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,11 +1,11 @@
 import HeroAbout from "@/app/components/tentangkami/heroaboutus";
-import AboutContent from "@/app/components/tentangkami/AboutContent";
+import AboutContent from "@/app/components/tentangkami/BackgroundCanvas";
 
 export default function AboutPage() {
   return (
     <>
       <HeroAbout />
-      <AboutContent />
+      <BackgroundCanvas />
     </>
   );
 } 
