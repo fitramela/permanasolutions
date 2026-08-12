69e5a297127da1f8dfaa3caa359f8f01e355d221
fitramela <152989664+fitramela@users.noreply.github.com>
2026-08-12 17:16:19 +0700

    Update companysection.tsx

--- app/components/tentangkami/companysection.tsx
+++ app/components/tentangkami/companysection.tsx
@@ -1,61 +1,61 @@
-"use client";
+// "use client";
 
-import BackgroundCanvas from "./BackgroundCanvas";
-import OurProfile from "./OurProfile"
-import PermanaSolutions from "./PermanaSolutions";
-import VisionMission from "./visionmission";
-import  MeetTeam  from "./MeetTeam";
-import  TeamSection  from "./teamsection";
+// import BackgroundCanvas from "./BackgroundCanvas";
+// import OurProfile from "./OurProfile"
+// import PermanaSolutions from "./PermanaSolutions";
+// import VisionMission from "./visionmission";
+// import  MeetTeam  from "./MeetTeam";
+// import  TeamSection  from "./teamsection";
 
-export default function CompanySection() {
-  return (
-    <section
-  id="company"
-  className="
-    relative
-    overflow-hidden
-    -mt-[80px]
-    z-30
-  "
->
-      {/* Background Decoration */}
+// export default function CompanySection() {
+//   return (
+//     <section
+//   id="company"
+//   className="
+//     relative
+//     overflow-hidden
+//     -mt-[80px]
+//     z-30
+//   "
+// >
+//       {/* Background Decoration */}
 
-      <BackgroundCanvas />
+//       <BackgroundCanvas />
 
-      {/* Content */}
+//       {/* Content */}
 
-      <div
-        className="
-          relative
-          z-10
+//       <div
+//         className="
+//           relative
+//           z-10
 
-          mx-auto
+//           mx-auto
 
-          w-full
+//           w-full
 
-          max-w-[1440px]
+//           max-w-[1440px]
 
-          px-[100px]
+//           px-[100px]
 
-          xl:px-[100px]
+//           xl:px-[100px]
 
-          lg:px-[70px]
+//           lg:px-[70px]
 
-          md:px-[40px]
+//           md:px-[40px]
 
-          sm:px-6
-        "
-      >
-        <OurProfile />
+//           sm:px-6
+//         "
+//       >
+//         <OurProfile />
 
-        <PermanaSolutions />
+//         <PermanaSolutions />
 
-        <VisionMission />
+//         <VisionMission />
 
-        <MeetTeam />
+//         <MeetTeam />
 
-      </div>
-       <TeamSection />
-    </section>
-  );
-}
+//       </div>
+//        <TeamSection />
+//     </section>
+//   );
+// }
