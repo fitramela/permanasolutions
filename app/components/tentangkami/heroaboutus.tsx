af26086a0d285ecad70f8f6b04e00a07501e00fe
bimasyudhistira-lang <bimasyudhistira@gmail.com>
2026-08-12 18:14:05 +0700

    Update heroaboutus.tsx

--- app/components/tentangkami/heroaboutus.tsx
+++ app/components/tentangkami/heroaboutus.tsx
@@ -19,7 +19,7 @@ export default function HeroAboutUs() {
       <div className="absolute inset-0">
 
         <Image
-          src="/yupsi.png"
+          src="/putrii.png"
           alt="About Us"
           fill
           priority
